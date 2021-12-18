07308db452faa334657359c109a6c0dcc6034de5
walker-finlay <walker.finlay@gmail.com>
2021-12-17 19:06:40 -0500

    vals making it into storage; refactored listener

--- recipe.js
+++ recipe.js
@@ -4,6 +4,8 @@ need the url of the window to check storage */
 var [slug, ...preslug] = window.location.pathname.substring(1).split('/').reverse();
 console.log(`this is a recipe for ${slug}`);
 
+// TODO: Preload notes for this recipe
+
 const note_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-sticky" viewBox="0 0 16 16">
   <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293L9 13.793z"/>
 </svg>`;
@@ -17,33 +19,35 @@ const classnames = '.social-icons__list-item--bookmarkactivated,'
 const viewport_rect = window.visualViewport;
 const notebox = document.createElement('textarea');
 notebox.classList.add('notebox');
+notebox.hidden = true;
 
 notebox.onchange = e => {
-  let data = { slug: e.target.value };
+  let data = { [slug]: e.target.value };
   chrome.storage.sync.set(data)
-    .then(console.log('success'))
+    .then(console.log('fufilled'))
     .catch(err => console.log(err));
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   // Can't add buttons till the dom is constructed obv
   console.log('DOM fully loaded and parsed');
-
-  // This is hacky but that's ok
-  document.querySelectorAll(classnames).forEach(node => {
-    console.log('cloning button');
-    let tmp = node.cloneNode(true);
-    tmp.style.position = "relative";
-    let link = tmp.firstChild;
-    delete link.dataset.eventClick;
-    link.href = link.target = '';
-    link.title = link.ariaLabel = "Notes";
-    link.innerHTML = note_svg;
-    link.onclick = noteButtonClickHandler;
-    tmp.appendChild(notebox);
-    node.parentNode.insertBefore(tmp, node.nextSibling)
-  });
+  document.querySelectorAll(classnames).forEach(createNoteButton);
 });
+
+function createNoteButton(node) {
+  // This is hacky but that's ok
+  console.log('cloning button');
+  let tmp = node.cloneNode(true);
+  tmp.style.position = "relative";
+  let link = tmp.firstChild;
+  delete link.dataset.eventClick;
+  link.href = link.target = '';
+  link.title = link.ariaLabel = "Notes";
+  link.innerHTML = note_svg;
+  link.onclick = noteButtonClickHandler;
+  tmp.appendChild(notebox);
+  node.parentNode.insertBefore(tmp, node.nextSibling)
+}
 
 function noteButtonClickHandler(e) {
   e.preventDefault();
