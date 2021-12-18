2c823bb1a54fbf2463b2069e03aee2eecca49edf
walker-finlay <walker.finlay@gmail.com>
2021-12-18 01:50:46 -0500

    quick save

--- recipe.js
+++ recipe.js
@@ -18,11 +18,35 @@ const note_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
 const note_svg_filled = `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-sticky-fill" viewBox="0 0 16 16">
   <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zm6 8.5a1 1 0 0 1 1-1h4.396a.25.25 0 0 1 .177.427l-5.146 5.146a.25.25 0 0 1-.427-.177V9.5z"/>
 </svg>`;
+const edit_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>`;
+const delete_svg_filled = `
+<svg xmlns="http://www.w3.org/2000/svg" width=16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg>`;
 const add_note_color = `rgb(201, 201, 201)`;
 const view_note_color = `rgb(51, 51, 51)`;
 const section_delimiter_color = `rgb(226, 221, 204)`;
 const classnames = '.social-icons__list-item--bookmarkactivated,'
   + ' .social-icons__list-item--bookmark';
+const recipe_main_classname = `recipe__main-content`;
+const sticky_box_classname = `stick-box__primary`;
+const notebox_options = [
+  {
+    title: 'Edit note',
+    classname: 'nb-edit',
+    icon: edit_svg,
+    clickhandler: noteEditHandler
+  },
+  {
+    title: 'Delete note',
+    classname: 'nb-delete',
+    icon: delete_svg_filled,
+    clickhander: noteDeleteHandler
+  }
+];
 const viewport_rect = window.visualViewport;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -30,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM fully loaded and parsed');
   document.querySelectorAll(classnames).forEach(node => {
     createNoteButton(node);
-
+    createNoteBox(node);
   });
 });
 
@@ -52,6 +76,35 @@ function noteButtonClickHandler(e) {
   console.log('clicked a note button');
 }
 
+function noteDeleteHandler(e) { }
+function noteEditHandler(e) { }
+
+/**
+ * Create a note box which unfolds down from mid & small screen buttons
+ * @param {Node} reference_node element which contains social list wrapper
+ */
+function createHorizontal(reference_node) {
+  let notebox = document.createElement('div');
+  notebox.classList.add('nb-container__horizontal');
+  notebox.appendChild(createNoteBoxWrapper(notebox_options));
+  reference_node.insertBefore(notebox, reference_node.firstChild.nextSibling);
+}
+/**
+ * Create a note box
+ * @param {Node} parent_node element which contains social list wrapper
+ */
+function createVertical(parent_node) { }
+
+function createNoteBox(node) {
+  let p = node.parentNode.parentNode.parentNode;
+  if (p.classList.contains(recipe_main_classname)) {
+    createHorizontal(p);
+  }
+  else if (p.classList.contains(sticky_box_classname)) {
+    createVertical(p);
+  }
+}
+
 /**
  * nb wrapper{
  *   nb menu {
@@ -59,19 +112,23 @@ function noteButtonClickHandler(e) {
  *   nb textarea}
  * @param {object} options includes id, icon, click handler
  */
-function createNoteBox(options) {
-  nb_wrapper = document.createElement('div');
-  nb_wrapper.classList.add('nb-conatiner');
-  nb_textarea = nb_wrapper.appendChild(document.createElement('textarea'));
-  nb_textarea.classList.add('nb-textarea');
-  nb_menubar = nb_wrapper.appendChild(document.createElement('div'));
+function createNoteBoxWrapper(options) {
+  let nb_wrapper = document.createElement('div');
+  nb_wrapper.classList.add('nb-wrapper');
+  let nb_menubar = nb_wrapper.appendChild(document.createElement('div'));
   nb_menubar.classList.add('nb-menubar');
+  let nb_textarea = nb_wrapper.appendChild(document.createElement('textarea'));
+  nb_textarea.placeholder = "Enter your note here!"
+  nb_textarea.classList.add('nb-textarea');
+  nb_textarea.maxWidth = null
   for (option of options) {
     let tmp = nb_menubar.appendChild(document.createElement('div'))
     tmp.classList.add(option.classname, 'nb-menubar-option');
     tmp.innerHTML = option.icon;
-    tmp.onclick = option.clickhandler
+    tmp.onclick = option.clickhandler;
+    tmp.title = option.title;
   }
+  return nb_wrapper;
 }
 
 /**
